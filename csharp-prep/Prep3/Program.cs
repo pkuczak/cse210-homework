c8492f3c2bdc29707fe1cfab90841e4543a7e0b3
pkuczak <paulkuczak@gmail.com>
2023-10-31 16:21:18 -0400

    Submitting Prep3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("Hello Prep3 World!");
+
+        // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+        string playAgain = "yes";
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 111);
+
+            Console.Write("What is your guess? ");
+            int userGuess = int.Parse(Console.ReadLine());
+
+            int count = 1;
+            
+            do
+            {
+                if (userGuess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (userGuess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                count++;
+                Console.Write("Guess again: ");
+                userGuess = int.Parse(Console.ReadLine());
+            } while (userGuess != magicNumber);
+
+            Console.WriteLine($"You guessed it! You made {count} guesses.");
+            Console.WriteLine();
+            Console.Write("Do you want to play agian? ");
+            playAgain = Console.ReadLine();
+            playAgain = playAgain.ToLower();
+        } while (playAgain == "yes");
     }
 }
